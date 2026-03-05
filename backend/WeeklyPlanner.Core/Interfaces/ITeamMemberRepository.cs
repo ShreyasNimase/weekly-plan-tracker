47d7c01b5ebcd1bb54f4b60ca2479746a0b09af6
Shreyas Nimase <nimaseshreyas3525@gmail.com>
2026-03-05 10:10:02 +0530

    feat(backend): add repository interface for team member operations

--- backend/WeeklyPlanner.Core/Interfaces/ITeamMemberRepository.cs
+++ backend/WeeklyPlanner.Core/Interfaces/ITeamMemberRepository.cs
@@ -5,4 +5,9 @@ namespace WeeklyPlanner.Core.Interfaces;
 public interface ITeamMemberRepository
 {
     Task<TeamMember> AddAsync(TeamMember teamMember);
+    Task<IEnumerable<TeamMember>> GetAllActiveAsync();
+    Task<IEnumerable<TeamMember>> GetAllAsync();
+    Task<TeamMember?> GetByIdAsync(Guid id);
+    Task<TeamMember> UpdateAsync(TeamMember teamMember);
+    Task<bool> IsInActiveCycleAsync(Guid id);
 }
