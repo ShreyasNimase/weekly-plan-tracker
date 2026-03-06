3e0097e143854b7e4cf5ddd5f88a55a5ed134d94
Shreyas Nimase <nimaseshreyas3525@gmail.com>
2026-03-06 10:53:22 +0530

    feat(infra): add EF Core initial migration for updated schema

--- backend/WeeklyPlanner.Infrastructure/Migrations/20260305233841_InitialCreate.Designer.cs
+++ backend/WeeklyPlanner.Infrastructure/Migrations/20260305233841_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WeeklyPlanner.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using WeeklyPlanner.Infrastructure.Data;
 namespace WeeklyPlanner.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260305233841_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
